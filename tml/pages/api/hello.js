9b88af16a3e19b2f41b256157debcf7adecba7d6
monkhzul <38126118+monkhzul@users.noreply.github.com>
2022-08-18 17:02:44 +0800

    commit

--- tml/pages/api/hello.js
+++ tml/pages/api/hello.js
@@ -2,13 +2,8 @@ import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient();
 
-export default function Main(req, res) {
-  const customer = prisma.$queryRaw`SELECT [TradeShopId],[Name],[FullAddress],[DateRemove] FROM [SMTTerms].[dbo].[t_TradeShops]`
+export const Main = async (req, res) => {
+  const customer = await prisma.$queryRaw`SELECT [TradeShopId],[Name],[FullAddress],[DateRemove] FROM [SMTTerms].[dbo].[t_TradeShops]`
 
-  if (customer) {
-    res.status(200).send(customer);
-  } else {
-    res.status(404).json({ message: "Couldn't find customer." });
-    return;
-  }
+ return customer
 }
